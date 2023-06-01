3a1b4a3e28f31af99e07d34f7fa0cf3e09c3d1c3
Nasuton <lovetennis_airu@yahoo.co.jp>
2023-06-01 20:15:47 +0900

    MicroSoftGraphAPI_Delegated Add

---
+++ MicroSoftGraphAPI_Delegated/MSGraphAPI.cs
@@ -0,0 +1,155 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace TestCSharp
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            var msGraph = new MSGraphAPI();
+            msGraph.DelegatedPermission("取得対象となる会議室のメールアドレス").Wait();
+        }
+    }
+
+    public class TokenResponse
+    {
+        public string access_token { get; set; }
+    }
+
+    public class AppData
+    {
+        [JsonProperty("value")]
+        public List<Calendar> Value { get; set; }
+        [JsonProperty("@odata.nextLink")]
+        public string odata_nextLink { get; set; }
+
+        public bool ContainsNextLink()
+        {
+            return !string.IsNullOrEmpty(odata_nextLink);
+        }
+
+        public string GetNextLink()
+        {
+            return odata_nextLink;
+        }
+    }
+
+    public class Schedule
+    {
+        [JsonProperty("dateTime")]
+        public DateTime dateTime { get; set; }
+
+        [JsonProperty("timeZone")]
+        public string timeZone { get; set; }
+    }
+
+    public class Organizer
+    {
+        [JsonProperty("emailAddress")]
+        public EmailAddress emailAddress { get; set; }
+    }
+
+    public class Attendees
+    {
+        [JsonProperty("emailAddress")]
+        public EmailAddress emailAddress { get; set; }
+    }
+
+    public class EmailAddress
+    {
+        [JsonProperty("name")]
+        public string name { get; set; }
+        [JsonProperty("address")]
+        public string address { get; set; }
+    }
+
+    public class Calendar
+    {
+        
+        public string subject { get; set; }
+        [JsonProperty("start")]
+        public Schedule start { get; set; }
+        [JsonProperty("end")]
+        public Schedule end { get; set; }
+        [JsonProperty("organizer")]
+        public Organizer organizer { get; set; }
+        [JsonProperty("attendees")]
+        public List<Attendees> attendees { get; set; }
+    }
+
+    internal class MSGraphAPI
+    {
+        private readonly string tenantName = "アプリが登録されたテナントID";
+
+        private readonly string clientUserid = "作成したAzure上のアプリID";
+        private readonly string clientUserSecret = "Azure上のアプリで作成したクライアントシークレットキーの値";
+        private readonly string userName = "認証時に使用するユーザーアドレス(二段階認証の設定がされていないもの)";
+        private readonly string userPass = "userNameのパスワード";
+        private Dictionary<string, string> reqUserTokenBody = null;
+
+        public MSGraphAPI()
+        {
+            reqUserTokenBody = new Dictionary<string, string>
+            {
+                { "grant_type", "password" },
+                {"username", userName },
+                {"password", userPass },
+                { "scope", "Calendars.Read Calendars.Read.Shared" }, // Azureアプリに含まれていないとエラーが発生する
+                { "client_id", clientUserid },
+                { "client_secret", clientUserSecret }
+            };
+        }
+
+        /// <summary>
+        /// 委任されたアクセス許可バージョン
+        /// </summary>
+        /// <param name="_accountId">対象メールアドレス</param>
+        /// <returns></returns>
+        public async Task DelegatedPermission(string _accountId)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                string tokenEndpoint = $"https://login.microsoftonline.com/{tenantName}/oauth2/v2.0/token";
+                var tokenResponse = await client.PostAsync(tokenEndpoint, new FormUrlEncodedContent(reqUserTokenBody));
+                var tokenContent = await tokenResponse.Content.ReadAsStringAsync();
+                var tokenAppResponse = JsonConvert.DeserializeObject<TokenResponse>(tokenContent);
+
+                try
+                {
+                    string apiUrl = $"https://graph.microsoft.com/v1.0/users/{_accountId}/calendarView?startDateTime=2023-01-01T19:00:00&endDateTime=2023-05-31T00:00:00&$select=subject,start,end,organizer,attendees";
+                    while (true)
+                    {
+                        var request = new HttpRequestMessage(HttpMethod.Get, apiUrl);
+                        request.Headers.Add("Authorization", $"Bearer {tokenAppResponse.access_token}");
+
+                        var response = await client.SendAsync(request);
+                        var responseData = await response.Content.ReadAsStringAsync();
+                        var appData = JsonConvert.DeserializeObject<AppData>(responseData);
+                        var appCalendars = appData.Value;
+                        foreach (var calendar in appCalendars)
+                        {
+                            Console.WriteLine(calendar.organizer.emailAddress.address);
+                        }
+
+                        if (!appData.ContainsNextLink())
+                        {
+                            break;
+                        }
+
+                        apiUrl = appData.GetNextLink();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+
+    }
+}
---
+++ MicroSoftGraphAPI_Delegated/MSGraphAPI.ps1
@@ -0,0 +1,41 @@
+﻿#######################GraphAPI接続 start
+$tenantName = "アプリが登録されたテナントID"
+$clientUserid = "作成したAzure上のアプリID"
+$clientUserSecret = "Azure上のアプリで作成したクライアントシークレットキーの値"
+$userName = "認証時に使用するユーザーアドレス(二段階認証の設定がされていないもの)"
+$userPass = "userNameのパスワード"
+
+$ReqUserTokenBody = @{
+    Grant_Type    = "password"
+    username      = $userName
+    password      = $userPass
+    client_Id     = $clientUserid
+    Client_Secret = $clientUserSecret
+    Scope         = "Calendars.Read Calendars.Read.Shared" # Azureアプリに含まれていないとエラーが発生する
+}
+
+$TokenUserResponse = Invoke-RestMethod -Uri "https://login.microsoftonline.com/$tenantName/oauth2/v2.0/token" -Method POST -Body $ReqUserTokenBody
+#######################GraphAPI接続 end
+
+$TargetRoom = "取得対象となる会議室のメールアドレス"
+
+#######################GraphAPIで情報取得(アプリケーション委任版)
+try {
+    $apiUrl = 'https://graph.microsoft.com/v1.0/users/' + $TargetRoom + '/calendarView?startDateTime=2023-01-01T00:00:00&endDateTime=2023-05-31T23:59:59&$select=subject,start,end,organizer,attendees'
+    while ($true) {
+        $User_Data = Invoke-RestMethod -Headers @{Authorization = "Bearer $($TokenUserResponse.access_token)"} -Uri $apiUrl -Method Get
+        $User_Calendars = ($User_Data | select-object Value ).Value
+        $User_Calendars
+
+        $NextLink = $User_Data.'@odata.nextLink'
+        $apiUrl = $NextLink
+        if ($null -eq $NextLink) {
+            break;
+        }
+    }
+    
+} catch {
+    Write-Host $_.Exception
+}
+#######################GraphAPIで情報取得 end
+
