d04278a87fa78ee5f1bffded94c7a64da3382766
Nasuton <lovetennis_airu@yahoo.co.jp>
2023-09-22 10:44:44 +0900

    Aggregation Edit

--- Aggregation/Aggregation.cs
+++ Aggregation/Aggregation.cs
@@ -4,7 +4,7 @@ using System.IO;
 using System.Linq;
 
 /// <summary>
-/// 任意の確率で結果を排出
+/// アイテム作成日時集計
 /// </summary>
 namespace TestCSharp
 {
@@ -26,10 +26,12 @@ namespace TestCSharp
         {
             Console.WriteLine($"対象データ数：{itemCreateTimes.Count}");
             // 集計データ確認用
-            int dicCount = 0;
+            int dicCount1 = 0;
+            int dicCount2 = 0;
 
             // アイテムを30分ごとのスロットに分類する用変数
-            Dictionary<DateTime, int> slotCounts = new Dictionary<DateTime, int>();
+            Dictionary<DateTime, int> slotCounts1 = new Dictionary<DateTime, int>();
+            Dictionary<DateTime, int> slotCounts2 = new Dictionary<DateTime, int>();
             var dateTime = currentTime;
 
             // 現時刻から8時間前を取得
@@ -47,63 +49,78 @@ namespace TestCSharp
 
             foreach (DateTime itemCreateTime in itemCreateTimes)
             {
-                // 実行時間からの30分刻み版
                 foreach (DateTime timeSlot in timeSlots)
                 {
                     if (itemCreateTime >= timeSlot && itemCreateTime < timeSlot.AddMinutes(30))
                     {
-                        if (!slotCounts.ContainsKey(timeSlot))
+                        if (!slotCounts1.ContainsKey(timeSlot))
                         {
-                            slotCounts[timeSlot] = 1;
-                            dicCount++;
+                            slotCounts1[timeSlot] = 1;
+                            dicCount1++;
                         }
                         else
                         {
-                            slotCounts[timeSlot]++;
-                            dicCount++;
+                            slotCounts1[timeSlot]++;
+                            dicCount1++;
                         }
                         break; // アイテムは1つの時間スロットにのみカウントされる
                     }
                 }
 
-                // 1時間の30分刻み版
-                //DateTime slotStartTime = new DateTime(itemCreateTime.Year, itemCreateTime.Month, itemCreateTime.Day, itemCreateTime.Hour, 0, 0);
-                //int minutes = itemCreateTime.Minute;
-                //// 30分より前か後か判定する
-                //int slotIndex = minutes / 30;
-                //DateTime slotKey = slotStartTime.AddMinutes(slotIndex * 30);
+                DateTime slotStartTime = new DateTime(itemCreateTime.Year, itemCreateTime.Month, itemCreateTime.Day, itemCreateTime.Hour, 0, 0);
+                int minutes = itemCreateTime.Minute;
+                // 30分より前か後か判定する
+                int slotIndex = minutes / 30;
+                DateTime slotKey = slotStartTime.AddMinutes(slotIndex * 30);
 
-                //if (!slotCounts.ContainsKey(slotKey))
-                //{
-                //    slotCounts[slotKey] = 1;
-                //    dicCount++;
-                //}
-                //else
-                //{
-                //    slotCounts[slotKey]++;
-                //    dicCount++;
-                //}
+                if (!slotCounts2.ContainsKey(slotKey))
+                {
+                    slotCounts2[slotKey] = 1;
+                    dicCount2++;
+                }
+                else
+                {
+                    slotCounts2[slotKey]++;
+                    dicCount2++;
+                }
             }
 
-            var sortedDictionary = new Dictionary<DateTime, int>();
+            var sortedDictionary1 = new Dictionary<DateTime, int>();
+            var sortedDictionary2 = new Dictionary<DateTime, int>();
             if (descending)
             {
                 // キーを昇順にソート
-                sortedDictionary = slotCounts
+                sortedDictionary1 = slotCounts1
+                    .OrderBy(kvp => kvp.Key)
+                    .ToDictionary(kvp => kvp.Key, kvp => kvp.Value);
+                sortedDictionary2 = slotCounts2
                     .OrderBy(kvp => kvp.Key)
                     .ToDictionary(kvp => kvp.Key, kvp => kvp.Value);
             }
             else
             {
                 // キーを降順にソート
-                sortedDictionary = slotCounts
+                sortedDictionary1 = slotCounts1
+                    .OrderByDescending(kvp => kvp.Key)
+                    .ToDictionary(kvp => kvp.Key, kvp => kvp.Value);
+
+                sortedDictionary2 = slotCounts2
                     .OrderByDescending(kvp => kvp.Key)
                     .ToDictionary(kvp => kvp.Key, kvp => kvp.Value);
             }
 
-            Console.WriteLine($"集計合計数：{dicCount}");
+            Console.WriteLine($"集計合計数(実行時間からの30分区切り版)：{dicCount1}");
+            Console.WriteLine($"集計合計数(1時間を30分ずつに区切った版)：{dicCount2}");
+            Console.WriteLine();
+            Console.WriteLine("集計結果 : 実行時間からの30分区切り版");
             // 集計結果の出力
-            foreach (var kvp in sortedDictionary)
+            foreach (var kvp in sortedDictionary1)
+            {
+                Console.WriteLine($"{kvp.Key}: {kvp.Value} アイテム");
+            }
+            Console.WriteLine();
+            Console.WriteLine("集計結果 : 1時間を30分ずつに区切った版");
+            foreach (var kvp in sortedDictionary2)
             {
                 Console.WriteLine($"{kvp.Key}: {kvp.Value} アイテム");
             }
@@ -115,7 +132,7 @@ namespace TestCSharp
         /// <param name="csvPath">作成先CSVパス</param>
         public void TargetCSVDateCreate(string csvPath)
         {
-            if (csvPath != null)
+            if (csvPath != null && csvPath != string.Empty)
             {
                 List<string[]> csvData = new List<string[]>();
 
@@ -131,7 +148,7 @@ namespace TestCSharp
                     dateTime = dateTime.AddMinutes(-minutesToSubtract);
                     string[] row = new string[] { dateTime.ToString("yyyy/MM/dd HH:mm:ss") };
                     csvData.Add(row);
-                    Console.WriteLine($"引いた時間: {minutesToSubtract}分, 残り時間: {(targetTime - dateTime).TotalMinutes}分");
+                    Console.WriteLine($"引いた時間: {minutesToSubtract}分, 残り時間: {(dateTime - targetTime).TotalMinutes}分");
                 }
 
                 try
@@ -147,6 +164,7 @@ namespace TestCSharp
                     }
 
                     Console.WriteLine($"CSVファイル '{csvPath}' にデータを書き込みました。");
+                    Console.WriteLine();
                 }
                 catch (Exception ex)
                 {
@@ -198,15 +216,17 @@ namespace TestCSharp
         {
             var agger = new Aggregation();
             var datetimeList = new List<DateTime>();
-            string csvPath = @"CSVまでのパス(テスト用CSVファイル作成パス)";
+            string csvPath = @".\CreateTime.csv";
             // テストデータ作成
             agger.TargetCSVDateCreate(csvPath);
             // データ集計
             var result = agger.ReadCsvFile(csvPath);
             foreach (var row in result)
             {
+                Console.WriteLine($"読み込んだCSVデータ : {row[0]}");
                 datetimeList.Add(DateTime.Parse(row[0]));
             }
+            Console.WriteLine();
             // 昇順にソートして実施
             Console.WriteLine("昇順");
             agger.AggregationCount(datetimeList);
